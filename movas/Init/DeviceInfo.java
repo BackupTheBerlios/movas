19488fcc9bdc36c3ed2dfffdabf26ab409fa03c9
cvw <cvw>
2003-12-03 13:49:57 +0000

    *** empty log message ***

---
+++ movas/Init/DeviceInfo.java
@@ -0,0 +1,100 @@
+/*
+ * DeviceInfo.java
+ *
+ * Created on 3. Dezember 2003, 12:01
+ */
+package movas.Init;
+
+import javax.media.*;
+import javax.media.format.*;
+import java.util.*;
+/**
+ *
+ * @author  schulzma
+ */
+public class DeviceInfo {
+    
+    private java.util.Vector deviceListVector = null;
+    
+    /** Creates a new instance of DeviceInfo */
+    public DeviceInfo() {
+        deviceListVector = CaptureDeviceManager.getDeviceList(null);
+    }
+    
+
+     public java.util.Vector [] getDevices() {   
+        java.util.Vector [] deviceNames = new java.util.Vector[2];
+        deviceNames[0] = new Vector();
+        deviceNames[1] = new Vector();
+        for (int x = 0; x < deviceListVector.size(); x++)
+            {
+                // get all device name
+                CaptureDeviceInfo deviceInfo = (CaptureDeviceInfo) deviceListVector.elementAt(x);
+                String deviceInfoText = deviceInfo.getName();
+
+                Format deviceFormat[] = deviceInfo.getFormats();
+
+                if (deviceFormat[0] instanceof VideoFormat) {
+                    //System.out.println("Video: "+deviceInfo.getName());
+                    deviceNames[0].add(deviceInfo.getName());
+                }
+                // serach for audio device
+                if (deviceFormat[0] instanceof AudioFormat) {
+                    //System.out.println("Audio: "+deviceInfo.getName());
+                    deviceNames[1].add(deviceInfo.getName());
+                }
+            }
+        return deviceNames;
+     }
+    
+     
+     
+     public javax.media.CaptureDeviceInfo getVideoDevice(String _VideoDeviceName) {
+         CaptureDeviceInfo captureVideoDevice = null;
+
+         for (int x = 0; x < deviceListVector.size(); x++)
+	 {
+            // display device name
+            CaptureDeviceInfo deviceInfo = (CaptureDeviceInfo) deviceListVector.elementAt(x);
+            String deviceInfoText = deviceInfo.getName();
+            if (deviceInfo.getName().indexOf(_VideoDeviceName) >= 0)
+                captureVideoDevice = deviceInfo;            
+         } // for (int x = 0; x < deviceListVector.size(); x++)
+         return captureVideoDevice;
+     }
+     
+     public javax.media.CaptureDeviceInfo getAudioDevice(String _AudioDeviceName) {
+         CaptureDeviceInfo captureVideoDevice = null;
+
+         for (int x = 0; x < deviceListVector.size(); x++)
+	 {
+            // display device name
+            CaptureDeviceInfo deviceInfo = (CaptureDeviceInfo) deviceListVector.elementAt(x);
+            String deviceInfoText = deviceInfo.getName();
+            if (deviceInfo.getName().indexOf(_AudioDeviceName) >= 0)
+                captureVideoDevice = deviceInfo;   
+         } // for (int x = 0; x < deviceListVector.size(); x++)
+         return captureVideoDevice;
+     }
+    
+     public java.util.Vector getSupportedFormats(CaptureDeviceInfo _device) {
+         Format deviceFormat[] = _device.getFormats();
+         java.util.Vector formats = new java.util.Vector();
+         for (int y = 0; y < deviceFormat.length; y++)
+         {
+            formats.add(deviceFormat[y].toString());
+            //System.out.println(deviceFormat[y].toString());
+         }
+         return formats;
+     }
+     
+     
+/*     
+      public static void main(String args[]) {
+        
+        new DeviceInfo().getDevices();
+        new DeviceInfo().getSupportedFormats(new DeviceInfo().getAudioDevice("JavaSound audio capture"));
+   
+    }
+*/   
+}
